01af64ca2f975f7f864ba2af833a9ccc3e542988
steelydylan <ess_president@me.com>
2014-08-20 22:57:31 +0900

    できてきたぞ

--- Moon.js
+++ Moon.js
@@ -47,18 +47,20 @@
     };
     /*value binding*/
     $(document).on("input","[value-binding]",function(){
-        var id = $(this).attr("value-binding");
-        var parent = $(this).data("parent");
-        var obj = Moon.find(parent);
-        obj.data[id] = $(this).val();
+        var id = $(this).data("id");
+        var splits = id.split(".");
+        var parentId = splits[0];
+        var obj = Moon.find(parentId);
+        obj.data[splits[1]] = $(this).val();
         obj.update();
     });
     /*action binding*/
     $(document).on("click","[action-binding]",function(){
-        var id = $(this).attr("action-binding");
-        var parent = $(this).data("parent");
-        var obj = Moon.find(parent);
-        obj.data.actions[id].call(obj,this);
+        var id = $(this).data("action-id");
+        var splits = id.split(".");
+        var parentId = splits[0];
+        var obj = Moon.find(parentId);
+        obj.getDataByString(obj.getPathFromId(id)).call(obj,this);
     });
     Moon.ObjectController = Moon.createClass({
         initialize:function(id,obj){
@@ -76,16 +78,21 @@
                 html = html.replaceAll("{{/if}}","</span>");
                 /*変数をdomに変換*/
                 for(var key in obj){
-                    html = html.replaceAll("{{"+key+"}}","<span data-parent='"+id+"' data-id='"+key+"'></span>");
-                    html = html.replaceAll("{{@"+key+"}}","<span data-parent='"+id+"' data-id='_"+key+"'></span>");
+                    html = html.replaceAll("{{"+key+"}}","<span data-id='"+id+"."+key+"'></span>");
+                    html = html.replaceAll("{{@"+key+"}}","<span data-id='"+id+"[]."+key+"'></span>");
                     /*ifブロックをdomに変換*/
-                    html = html.replaceAll("{{#if "+key+"}}","<span data-if='"+key+"'>");
-                    html = html.replaceAll("{{#unless "+key+"}}","<span data-unless='"+key+"'>");
+                    html = html.replaceAll("{{#if "+key+"}}","<span data-if='"+id+"."+key+"'>");
+                    html = html.replaceAll("{{#unless "+key+"}}","<span data-unless='"+id+"."+key+"'>");
                 }
                 $(this).html(html);
             });
-            this.$.find("[value-binding],[action-binding]").each(function(){
-                $(this).attr("data-parent",id);
+            this.$.find("[value-binding]").each(function(){
+                var childId = $(this).attr("value-binding");
+                $(this).attr("data-id",id+"."+childId);
+            });
+            this.$.find("[action-binding]").each(function(){
+                var childID = $(this).attr("action-binding");
+                $(this).attr("data-action-id",id+"."+childID);
             });
             var i = 0;
             var that = this;
@@ -101,8 +108,54 @@
             i = Number(i);
             return this.whileHtml[i].html;
         },
+        getDataByString:function(s){
+            var o = this.data;
+            s = s.replace(/\[(\w+)\]/g, '.$1');  // convert indexes to properties
+            s = s.replace(/^\./, ''); // strip leading dot
+            var a = s.split('.');
+            while (a.length) {
+                var n = a.shift();
+                if (n in o) {
+                    o = o[n];
+                } else {
+                    return;
+                }
+            }
+            return o;
+        },
+        remove:function(data){
+            var id = $(data).data("id") || $(data).data("action-id");
+            id = this.getPathFromId(id);
+            this.removeDataByString(id);
+            this.update();
+        },
+        updateDataByString:function(path){
+            var object = this.data;
+            var stack = path.split('.');
+            while(stack.length>1){
+                object = object[stack.shift()];
+            }
+            object[stack.shift()] = newValue;
+        },
+        removeDataByString:function(path){
+            var object = this.data;
+            var stack = path.split('.');
+            while(stack.length>1){
+                object = object[stack.shift()];
+            }
+            var shift = stack.shift();
+            if(shift.match(/^\d+$/)){
+                object.splice(Number(shift),1);
+            }else{
+                delete object[shift];
+            }
+        },
         getId:function(){
             return this.$.data("id");
+        },
+        getPathFromId:function(path){
+            var id = this.$.data("id");
+            return path.replace(id+".","");
         },
         getValue:function(value){
             /*関数なら計算結果をvalueに格納*/
@@ -113,16 +166,18 @@
             return data;
         },
         update:function(input){
-            var id = this.getId();
+            var parentID = this.getId();
             var data = this.data;
             var that = this;
             this.$.find("[data-id]").each(function(){
                 var id = $(this).data("id");
+                id = id.replace(parentID+".","");
                 var value = that.getValue(data[id]);
                 $(this).html(value);
             });
             this.$.find("[data-if]").each(function(){
                 var id = $(this).data("if");
+                id = id.replace(parentID+".");
                 var value = that.getValue(data[id]);
                 if(value){
                     $(this).show();
@@ -132,6 +187,7 @@
             });
             this.$.find("[data-unless]").each(function(){
                 var id = $(this).data("unless");
+                id = id.remove(parentID+".","");
                 var value = that.getValue(data[id]);
                 if(value){
                     $(this).hide();
@@ -141,19 +197,24 @@
             });
             this.$.find("[data-while]").each(function(){
                 var id = $(this).data("while");
+                id = id.replace(parentID+".","");
                 var contents = data[id];
                 var length = contents.length;
                 var index = $(this).data("index");
                 var html = that.getHtmlFromIndex(index);
-                var render = html;
-                for(var i = 0; i < length - 1; i++){
-                    render += html;
+                var render = "";
+                for(var i = 0; i < length; i++){
+                    var temp = html.replace("[]","."+id+"."+i);
+                    temp = temp.replace("data-action-id","data-id='"+parentID+"."+id+"."+i+"' data-action-id");
+                    render += temp;
                 }
                 $(this).html(render);
                 for(var i = 0; i < length; i++){
                     var content = contents[i];
                     for(var key in content){
-                        $(this).find("[data-id='_"+key+"']").eq(i).html(that.getValue(content[key]));
+                        var $item = $(this).find("[data-id='"+parentID+"."+id+"."+i+"."+key+"']");
+                        $item.html(that.getValue(content[key]));
+                        $item.attr("data-index",i);
                     }
                 }
             });
@@ -189,13 +250,6 @@
         add:function(key,value){
             var array = this.data[key];
             array.push(value);
-            this.update();
-        },
-        remove:function(key,item){
-            var array = this.data[key];
-            var index = $(item).index();
-            console.log(item);
-            array.splice(index-1,1);
             this.update();
         },
         get:function(key){
